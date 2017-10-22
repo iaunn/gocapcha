014d9bba52a27bfd19da5f077777e9ec8ff39a52
Apiwat Suksuwan <apiwat.s@zanroo.com>
2017-10-22 13:06:37 +0700

    init

---
+++ exersice_test.go
@@ -0,0 +1,16 @@
+package golang
+
+import (
+	"testing"
+)
+
+func TestCaptcha(t *testing.T) {
+	c := []int{1, 1, 1, 1}
+
+	rs := getCaptcha(c)
+
+	if rs != "1 + one" {
+		t.Error("it should be 1 + one")
+	}
+
+}
